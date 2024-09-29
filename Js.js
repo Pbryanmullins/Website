b4f046f115b9826ebbad301ebc2973509a157581
Pbryanmullins <130524275+Pbryanmullins@users.noreply.github.com>
2024-09-29 10:54:56 -0400

    Create Js.js

---
+++ Js.js
@@ -0,0 +1,4 @@
+// You can add interactive functionality here in the future, such as showing a modal for product details
+document.querySelector('.btn-buy').addEventListener('click', function() {
+    alert('Redirecting to the purchase page...');
+});
